dc1209a4fecd679373d8decbeeccd018adde03b3
Richard Schmidt <richardnew12511@gmail.com>
2022-05-20 12:29:22 -0500

    server index.html

--- server/src/app.js
+++ server/src/app.js
@@ -8,5 +8,8 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
